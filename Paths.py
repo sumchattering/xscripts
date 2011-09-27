7281e9784e966cf1d5e40873970c8138b1154a9e
samyzee <sumeru@ufl.edu>
2011-09-27 18:19:51 -0400

    Added a few awesome tools from Three20

---
+++ Paths.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Paths.py
+
+Created by Jeff Verkoeyen on 2010-10-18.
+"""
+
+import os
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+src_dir = os.path.dirname(script_dir)
